fc8e58474e18bdd5bf17c6a91e28ade48fe1ffa1
VSRUJANA <srujana.valavala@gmail.com>
2020-11-19 18:30:33 +0530

    [srujana] Add. LeapYear.js to check if given year is leap year added

---
+++ IfElse Practice/LeapYear.js
@@ -0,0 +1,7 @@
+// 3. Checking for leap year
+var year = prompt('Enter year to check for leap year : ');
+var isLeapYear = (year % 100 == 0) ? (year % 400 == 0) : (year % 4 == 0);
+if (isLeapYear)
+    console.log(year + " is a Leap year");
+else
+    console.log(year + " is not a Leap year");
