93a06222223c238ea0f37148531bd5b5a311f044
VSRUJANA <srujana.valavala@gmail.com>
2020-11-19 19:11:36 +0530

    [srujana] Add. DayOfWeek.js to read a number and display WeekDay added

---
+++ SwitchCase Practice/DayOfWeek.js
@@ -0,0 +1,30 @@
+// 2. Read a number and display the Week day
+const prompt = require('prompt-sync')();
+const inputNumber = Number(prompt('Enter a single digit number : '));
+switch(inputNumber)
+{
+case 1:
+    console.log("Monday");
+    break;
+case 2:
+    console.log("Tuesday");
+    break;
+case 3:
+    console.log("Wednesday");
+    break;
+case 4:
+    console.log("Thursday");
+    break;
+case 5:
+    console.log("Friday");
+    break;
+case 6:
+    console.log("Saturday");
+    break;
+case 7:
+        console.log("Sunday");
+        break;
+default:
+    console.log("Invalid Input");
+    break;
+}
