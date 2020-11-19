c9934b753f706978b72265acf9000c9519834ab1
VSRUJANA <srujana.valavala@gmail.com>
2020-11-19 18:31:19 +0530

    [srujana] Add. CoinToss.js to simulate coin toss added

---
+++ IfElse Practice/CoinToss.js
@@ -0,0 +1,7 @@
+// 4. Simulate Coin toss
+console.log("Simulating Coin flip : ")
+let v = Math.floor(Math.random() * 10) % 2;
+if (v == 0)
+    console.log("Heads");
+else
+    console.log("Tails");
