69cdf8e3b207eda6cd3f3bb9c7172dd3a933d380
VSRUJANA <srujana.valavala@gmail.com>
2020-11-19 18:41:23 +0530

    [srujana] Add. NumberInWord.js to write single digit number in word added

--- IfElse Practice/LeapYear.js
+++ IfElse Practice/LeapYear.js
@@ -1,4 +1,5 @@
 // 3. Checking for leap year
+const prompt = require('prompt-sync')();
 var year = prompt('Enter year to check for leap year : ');
 var isLeapYear = (year % 100 == 0) ? (year % 400 == 0) : (year % 4 == 0);
 if (isLeapYear)
---
+++ IfElseIfAndElse Practice/NumberInWord.js
@@ -0,0 +1,43 @@
+// 1. Read single digit number and write the number in word
+const prompt = require('prompt-sync')();
+const inputNumber = prompt('Enter a single digit number : ');
+if (inputNumber == 1) 
+{
+    console.log("One");
+}
+else if (inputNumber == 2)
+{
+    console.log("Two");
+}
+else if (inputNumber == 3) 
+{
+    console.log("Three");
+}
+else if (inputNumber == 4) 
+{
+    console.log("Four");
+}
+else if (inputNumber == 5) 
+{
+    console.log("Five");
+}
+else if (inputNumber == 6) 
+{
+    console.log("Six");
+}
+else if (inputNumber == 7) 
+{
+    console.log("Seven");
+}
+else if (inputNumber == 8) 
+{
+    console.log("Eight");
+}
+else if (inputNumber == 9) 
+{
+    console.log("Nine");
+}
+else 
+{
+    console.log("Invalid Number");
+}
