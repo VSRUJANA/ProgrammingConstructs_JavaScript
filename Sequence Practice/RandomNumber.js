286ef8ad4a0dc6c17e190ef3e3ae1a0d7647efa9
VSRUJANA <srujana.valavala@gmail.com>
2020-11-19 18:12:24 +0530

    [srujana] Add. RandomNumber.js to generate singledigit random number added

---
+++ Sequence Practice/RandomNumber.js
@@ -0,0 +1,4 @@
+// 1. To get single digit using random function
+let number = Math.floor(Math.random() * 10);
+console.log("Single digit random number : " + number);
+console.log('-'.repeat(process.stdout.columns));
