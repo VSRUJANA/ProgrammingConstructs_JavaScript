2f3f3e44f1d49bffbee2f9e37dc68d4734550a0d
VSRUJANA <srujana.valavala@gmail.com>
2020-11-19 23:08:36 +0530

    [srujana] Add. StorePrimeFactors.js to store prime factors in array added

---
+++ Array Practice/StorePrimeFactors.js
@@ -0,0 +1,23 @@
+// 3. store all the Prime Factors of a number n into an array
+const Prompt = require('prompt-sync')();
+let num = Number(Prompt('Enter Number : '));
+let arr = [];
+for (let i = 2; i <= num; i++) 
+{
+    if (num % i === 0) 
+    {
+        if (isPrime(i))
+            arr.push(i);
+    }
+}
+console.log(arr);
+function isPrime(num) 
+{
+    let is_prime = true;
+    for (let i = 2; i <= Math.sqrt(num); i++) 
+    {
+        if (num % i === 0)
+            is_prime = false;
+    }
+    return is_prime;
+}
